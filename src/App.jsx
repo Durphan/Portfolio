a14669b48d6e2cd2028db3da439d4100d060b56d
Esteban <estebandurante194@gmail.com>
2024-02-27 13:54:08 -0300

    fix: Fixing react router's problems

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
-import { Browser } from './route/browser'
+import { Home } from './pages/home/home'
+import { Skills } from './pages/skills/skills'
+import { Projects } from './pages/projects/projects'
+import { Studies } from './pages/studies/studies'
+
 
 function App() {
 
   return (
     <>
     <BrowserRouter>
-    <Browser/>
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/studies" element={<Studies />} />
+        </Routes>
     </BrowserRouter>
     </>
   )
