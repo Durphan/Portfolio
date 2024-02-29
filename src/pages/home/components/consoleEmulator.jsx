40b2babbb6607afb63f1c8a316065a3072b88c30
Esteban <estebandurante194@gmail.com>
2024-02-29 12:13:20 -0300

    Revert "feat: Added scroll behaviour and removed unnecessary tags"

--- src/pages/home/components/consoleEmulator.jsx
+++ src/pages/home/components/consoleEmulator.jsx
@@ -23,18 +23,25 @@ export const ConsoleEmulator = () => {
         width: isMobile ? "100%" : "70%",
         margin: "0 auto",
         overflowY: "auto",
-        fontFamily: "monospace",
-        height: "100%",
-        backgroundColor: "black",
-        color: "white",
-        padding: "10px",
-        maxWidth: "800px",
-        marginBottom: "30px",
       }}
     >
+      <Box
+        sx={{
+          backgroundColor: "black",
+          color: "white",
+          padding: "10px",
+          fontFamily: "monospace",
+          height: "100%",
+          width: "100%",
+          maxWidth: "800px",
+          margin: "0 auto",
+          marginBottom: "30px",
+        }}
+      >
         {output.map((content, index) => (
-          <div key={index} >{content}</div>
+          <Typography key={index}>{content}</Typography>
         ))}
       </Box>
+    </Box>
   );
 };
