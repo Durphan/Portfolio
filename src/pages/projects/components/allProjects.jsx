2ba2ff1b48c74289c50d8c33defe7cfabc8f5a91
Durphan <estebandurante194@gmail.com>
2024-07-08 19:54:12 -0300

    fix: random fixes

--- src/pages/projects/components/allProjects.jsx
+++ src/pages/projects/components/allProjects.jsx
@@ -1,6 +1,4 @@
 import { GlobalGameJam } from './globalGameJam';
-import { RickAndMorty } from './rickAndMorty';
-import { SunriseCafe } from './sunriseCafe';
 
 export const AllProjects = () => {
 	return (
